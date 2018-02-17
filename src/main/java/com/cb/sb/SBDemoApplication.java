5c4ea056a3d93cf35b6176df654acc5f349490c3
chenbin <3434677021@qq.com>
2018-02-17 11:36:34 +0800

    springBoot

--- src/main/java/com/cb/sb/SBDemoApplication.java
+++ src/main/java/com/cb/sb/SBDemoApplication.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/***
+ Controller 同SpringMVC一样，是表明该类是一个MVC的Controller类
+ SpringBootApplication 注解主要是开启自动配置以及指定java配置类等作用，是一个组合注解
+ SpringBootApplication注解中包含SpringBootConfiguration，而SpringBootConfiguration中包含@Configuration，所以可以省略@Configuration注解
+ */
 @Controller
 @SpringBootApplication
 public class SBDemoApplication {
