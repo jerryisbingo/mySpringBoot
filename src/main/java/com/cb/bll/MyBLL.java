a042f3951d5ef068a5713971b90008d57719b3c2
chenbin <3434677021@qq.com>
2018-02-27 16:04:37 +0800

    springBoot

---
+++ src/main/java/com/cb/bll/MyBLL.java
@@ -0,0 +1,12 @@
+package com.cb.bll;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyBLL {
+
+    public String sayHello(){
+        return "MyService Hello";
+    }
+
+}
--- src/main/java/com/cb/sb/SBDemoApplication.java
+++ src/main/java/com/cb/sb/SBDemoApplication.java
@@ -1,10 +1,14 @@
 package com.cb.sb;
 
+import com.cb.bll.MyBLL;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /***
  Controller 同SpringMVC一样，是表明该类是一个MVC的Controller类
@@ -13,12 +17,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @SpringBootApplication
+@ComponentScan(value = {"com.cb.sb","com.cb.bll"}) //默认简便main入口，只扫描本包及其子包，可以通过ComponentScan数组形式指定扫描哪些包
 public class SBDemoApplication {
+
+    @Autowired MyBLL myBLL;
 
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(){
-        return "hello world";
+        return myBLL.sayHello();
     }
 
     //main方法是一个方便快速启动的入口，通过此入口可以不用通过部署到tomcat启动，其内部会会自动加载tomcat插件进行启动，项目真正部署到Web容器后可以此入口不再具有作用
